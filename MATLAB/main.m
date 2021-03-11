d21159164803fc0005827f98dd4abfef089df7cf
nguyen-ryan-216334336 <46363286+nguyen-ryan-216334336@users.noreply.github.com>
2021-03-10 19:59:32 -0500

    files

--- MATLAB/main.m
+++ MATLAB/main.m
@@ -10,12 +10,12 @@ stations = [
     1337935.78387858, 6070317.12914242, 1427877.31632779];
 
 % **************** enter filenames here **************************
-station1 = ['ALGO00CNA_2_24hr_kin.pos']; % ALGO
-station2 = ['DUBO00CAN_2_24hr_kin.pos']; % DUBO
-station3 = ['WHIT00CAN_2_24hr_kin.pos']; % WHIT
-station4 = ['GOLD00USA_2_24hr_kin.pos']; % GOLD
-station5 = ['WUHN00CHN_2_24hr_kin.pos']; % WUHN
-station6 = ['IISC00IND_2_24hr_kin.pos']; % IISC
+station1 = ['ALGO00CNA_2_24hr_kin.pos']; 
+station2 = ['DUBO00CAN_2_24hr_kin.pos']; 
+station3 = ['WHIT00CAN_2_24hr_kin.pos']; 
+station4 = ['GOLD00USA_2_24hr_kin.pos']; 
+station5 = ['WUHN00CHN_2_24hr_kin.pos']; 
+station6 = ['IISC00IND_2_24hr_kin.pos'];
 station7 = ['ALGO00CNA_2_24hr_static.pos']; 
 station8 = ['DUBO00CAN_2_24hr_static.pos']; 
 station9 = ['WHIT00CAN_2_24hr_static.pos']; 
@@ -34,12 +34,12 @@ station21 = ['WHIT00CAN_2_30min_static.pos'];
 station22 = ['GOLD00USA_2_30min_static.pos']; 
 station23 = ['WUHN00CHN_2_30min_static.pos']; 
 station24 = ['IISC00IND_2_30min_static.pos']; 
-station25 = ['ALGO00CNA_3_24hr_kin.pos']; % ALGO
-station26 = ['DUBO00CAN_3_24hr_kin.pos']; % DUBO
-station27 = ['WHIT00CAN_3_24hr_kin.pos']; % WHIT
-station28 = ['GOLD00USA_3_24hr_kin.pos']; % GOLD
-station29 = ['WUHN00CHN_3_24hr_kin.pos']; % WUHN
-station30 = ['IISC00IND_3_24hr_kin.pos']; % IISC
+station25 = ['ALGO00CNA_3_24hr_kin.pos']; 
+station26 = ['DUBO00CAN_3_24hr_kin.pos']; 
+station27 = ['WHIT00CAN_3_24hr_kin.pos']; 
+station28 = ['GOLD00USA_3_24hr_kin.pos']; 
+station29 = ['WUHN00CHN_3_24hr_kin.pos']; 
+station30 = ['IISC00IND_3_24hr_kin.pos']; 
 station31 = ['ALGO00CNA_3_24hr_static.pos']; 
 station32 = ['DUBO00CAN_3_24hr_static.pos']; 
 station33 = ['WHIT00CAN_3_24hr_static.pos']; 
@@ -59,8 +59,44 @@ station46 = ['GOLD00USA_3_30min_static.pos'];
 station47 = ['WUHN00CHN_3_30min_static.pos']; 
 station48 = ['IISC00IND_3_30min_static.pos']; 
 
+%data lists
 data = {station1;station2;station3;station4;station5;station6;station7;station8;station9;station10;
     station11;station12;station13;station14;station15;station16;station17;station18;station19;station20;
     station21;station22;station23;station24;station25;station26;station27;station28;station29;station30;
     station31;station32;station33;station34;station35;station36;station37;station38;station39;station40;
-    station41;station42;station43;station44;station45;station46;station47;station48}
+    station41;station42;station43;station44;station45;station46;station47;station48};
+
+%stations
+stationALGO = {station1;station7;station13;station19;station25;station31;station37;station43};
+stationDUBO = {station2;station8;station14;station20;station26;station32;station38;station44};
+stationWHIT = {station3;station9;station15;station21;station27;station33;station39;station45};
+stationGOLD = {station4;station10;station16;station22;station28;station34;station40;station46};
+stationWUHN = {station5;station11;station17;station23;station29;station35;station41;station47};
+stationIISC = {station6;station12;station18;station24;station30;station36;station42;station48};
+
+%data arc
+station24H = {station1;station2;station3;station4;station5;station6;station7;station8;station9;station10;
+    station11;station12;station25;station26;station27;station28;station29;station30;
+    station31;station32;station33;station34;station35;station36};
+
+station30MIN = {station13;station14;station15;station16;station17;station18;station19;station20;
+    station21;station22;station23;station24;station37;station38;station39;station40;
+    station41;station42;station43;station44;station45;station46;station47;station48};
+
+%mode
+stationKIN = {station1;station2;station3;station4;station5;station6;station13;station14;station15;
+    station16;station17;station18;station25;station26;station27;station28;station29;station30;
+    station37;station38;station39;station40;station41;station42};
+
+stationSTAT = {station7;station8;station9;station10;station11;station12;station19;station20;
+    station21;station22;station23;station24;station31;station32;station33;station34;station35;
+    station36;station43;station44;station45;station46;station47;station48};
+
+%day
+stationD2 = {station1;station2;station3;station4;station5;station6;station7;station8;station9;station10;
+    station11;station12;station13;station14;station15;station16;station17;station18;station19;station20;
+    station21;station22;station23;station24};
+
+stationD3 = {station25;station26;station27;station28;station29;station30;
+    station31;station32;station33;station34;station35;station36;station37;station38;station39;station40;
+    station41;station42;station43;station44;station45;station46;station47;station48};
