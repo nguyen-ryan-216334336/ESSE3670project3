f4f0965af27aef21b4b01ef474f7a2e6de613020
Tim Kehoe <tim14@my.yorku.ca>
2021-03-10 20:45:11 -0500

    main update

--- MATLAB/main.m
+++ MATLAB/main.m
@@ -1,4 +1,4 @@
-function
+function [station24H] = main()
 
 % "stations" is composed of all station reference ECEF (XYZ) coordinates
 % station 1: ALGO, station 2: DUBO, station 3: WHIT, station 4: GOLD,
@@ -12,54 +12,54 @@ stations = [
     1337935.78387858, 6070317.12914242, 1427877.31632779];
 
 % **************** enter filenames here **************************
-station1 = ['ALGO00CNA_2_24hr_kin.pos']; 
-station2 = ['DUBO00CAN_2_24hr_kin.pos']; 
-station3 = ['WHIT00CAN_2_24hr_kin.pos']; 
-station4 = ['GOLD00USA_2_24hr_kin.pos']; 
-station5 = ['WUHN00CHN_2_24hr_kin.pos']; 
-station6 = ['IISC00IND_2_24hr_kin.pos'];
-station7 = ['ALGO00CNA_2_24hr_static.pos']; 
-station8 = ['DUBO00CAN_2_24hr_static.pos']; 
-station9 = ['WHIT00CAN_2_24hr_static.pos']; 
-station10 = ['GOLD00USA_2_24hr_static.pos']; 
-station11 = ['WUHN00CHN_2_24hr_static.pos']; 
-station12 = ['IISC00IND_2_24hr_static.pos'];
-station13 = ['ALGO00CNA_2_30min_kin.pos'];
-station14 = ['DUBO00CAN_2_30min_kin.pos']; 
-station15 = ['WHIT00CAN_2_30min_kin.pos']; 
-station16 = ['GOLD00USA_2_30min_kin.pos']; 
-station17 = ['WUHN00CHN_2_30min_kin.pos']; 
-station18 = ['IISC00IND_2_30min_kin.pos']; 
-station19 = ['ALGO00CNA_2_30min_static.pos']; 
-station20 = ['DUBO00CAN_2_30min_static.pos']; 
-station21 = ['WHIT00CAN_2_30min_static.pos']; 
-station22 = ['GOLD00USA_2_30min_static.pos']; 
-station23 = ['WUHN00CHN_2_30min_static.pos']; 
-station24 = ['IISC00IND_2_30min_static.pos']; 
-station25 = ['ALGO00CNA_3_24hr_kin.pos']; 
-station26 = ['DUBO00CAN_3_24hr_kin.pos']; 
-station27 = ['WHIT00CAN_3_24hr_kin.pos']; 
-station28 = ['GOLD00USA_3_24hr_kin.pos']; 
-station29 = ['WUHN00CHN_3_24hr_kin.pos']; 
-station30 = ['IISC00IND_3_24hr_kin.pos']; 
-station31 = ['ALGO00CNA_3_24hr_static.pos']; 
-station32 = ['DUBO00CAN_3_24hr_static.pos']; 
-station33 = ['WHIT00CAN_3_24hr_static.pos']; 
-station34 = ['GOLD00USA_3_24hr_static.pos']; 
-station35 = ['WUHN00CHN_3_24hr_static.pos']; 
-station36 = ['IISC00IND_3_24hr_static.pos'];
-station37 = ['ALGO00CAN_3_30min_kin.pos'];
-station38 = ['DUBO00CAN_3_30min_kin.pos']; 
-station39 = ['WHIT00CAN_3_30min_kin.pos']; 
-station40 = ['GOLD00USA_3_30min_kin.pos']; 
-station41 = ['WUHN00CHN_3_30min_kin.pos']; 
-station42 = ['IISC00IND_3_30min_kin.pos']; 
-station43 = ['ALGO00CNA_3_30min_static.pos']; 
-station44 = ['DUBO00CAN_3_30min_static.pos']; 
-station45 = ['WHIT00CAN_3_30min_static.pos']; 
-station46 = ['GOLD00USA_3_30min_static.pos']; 
-station47 = ['WUHN00CHN_3_30min_static.pos']; 
-station48 = ['IISC00IND_3_30min_static.pos']; 
+station1 = 'ALGO00CAN_2_24hr_kin.pos'; 
+station2 = 'DUBO00CAN_2_24hr_kin.pos'; 
+station3 = 'WHIT00CAN_2_24hr_kin.pos'; 
+station4 = 'GOLD00USA_2_24hr_kin.pos'; 
+station5 = 'WUHN00CHN_2_24hr_kin.pos'; 
+station6 = 'IISC00IND_2_24hr_kin.pos';
+station7 = 'ALGO00CAN_2_24hr_static.pos'; 
+station8 = 'DUBO00CAN_2_24hr_static.pos'; 
+station9 = 'WHIT00CAN_2_24hr_static.pos'; 
+station10 = 'GOLD00USA_2_24hr_static.pos'; 
+station11 = 'WUHN00CHN_2_24hr_static.pos'; 
+station12 = 'IISC00IND_2_24hr_static.pos';
+station13 = 'ALGO00CAN_2_30min_kin.pos';
+station14 = 'DUBO00CAN_2_30min_kin.pos'; 
+station15 = 'WHIT00CAN_2_30min_kin.pos'; 
+station16 = 'GOLD00USA_2_30min_kin.pos'; 
+station17 = 'WUHN00CHN_2_30min_kin.pos'; 
+station18 = 'IISC00IND_2_30min_kin.pos'; 
+station19 = 'ALGO00CAN_2_30min_static.pos'; 
+station20 = 'DUBO00CAN_2_30min_static.pos'; 
+station21 = 'WHIT00CAN_2_30min_static.pos'; 
+station22 = 'GOLD00USA_2_30min_static.pos'; 
+station23 = 'WUHN00CHN_2_30min_static.pos'; 
+station24 = 'IISC00IND_2_30min_static.pos'; 
+station25 = 'ALGO00CAN_3_24hr_kin.pos'; 
+station26 = 'DUBO00CAN_3_24hr_kin.pos'; 
+station27 = 'WHIT00CAN_3_24hr_kin.pos'; 
+station28 = 'GOLD00USA_3_24hr_kin.pos'; 
+station29 = 'WUHN00CHN_3_24hr_kin.pos'; 
+station30 = 'IISC00IND_3_24hr_kin.pos'; 
+station31 = 'ALGO00CAN_3_24hr_static.pos'; 
+station32 = 'DUBO00CAN_3_24hr_static.pos'; 
+station33 = 'WHIT00CAN_3_24hr_static.pos'; 
+station34 = 'GOLD00USA_3_24hr_static.pos'; 
+station35 = 'WUHN00CHN_3_24hr_static.pos'; 
+station36 = 'IISC00IND_3_24hr_static.pos';
+station37 = 'ALGO00CAN_3_30min_kin.pos';
+station38 = 'DUBO00CAN_3_30min_kin.pos'; 
+station39 = 'WHIT00CAN_3_30min_kin.pos'; 
+station40 = 'GOLD00USA_3_30min_kin.pos'; 
+station41 = 'WUHN00CHN_3_30min_kin.pos'; 
+station42 = 'IISC00IND_3_30min_kin.pos'; 
+station43 = 'ALGO00CAN_3_30min_static.pos'; 
+station44 = 'DUBO00CAN_3_30min_static.pos'; 
+station45 = 'WHIT00CAN_3_30min_static.pos'; 
+station46 = 'GOLD00USA_3_30min_static.pos'; 
+station47 = 'WUHN00CHN_3_30min_static.pos'; 
+station48 = 'IISC00IND_3_30min_static.pos'; 
 
 %data lists
 data = {station1;station2;station3;station4;station5;station6;station7;station8;station9;station10;
@@ -102,3 +102,16 @@ stationD2 = {station1;station2;station3;station4;station5;station6;station7;stat
 stationD3 = {station25;station26;station27;station28;station29;station30;
     station31;station32;station33;station34;station35;station36;station37;station38;station39;station40;
     station41;station42;station43;station44;station45;station46;station47;station48};
+
+% % RMSE for 24 hour data arc
+% RMSE_24hr = zeros(24,1);
+% 
+% for i = 1:24
+%     RMSE_24hr(i) = readPos(station24H{i});
+%     
+%       
+%     
+% end
+
+end
+
